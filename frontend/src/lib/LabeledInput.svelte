7493dd7d99907e677c59a4dac7569fb211cff744
Albert Latham <me@albertlatham.com>
2024-03-26 17:58:10 -0700

    wire up login
    using https://www.programonaut.com/how-to-use-pocketbase-auth-for-a-login-screen-step-by-step/\#pocketbase-auth

---
+++ frontend/src/lib/LabeledInput.svelte
@@ -0,0 +1,19 @@
+<script>
+	import { Label, Input } from 'flowbite-svelte';
+
+	export let label = '';
+	export let id = '';
+	export let type = 'text';
+	export let value = '';
+	export let placeholder = '';
+	export let helperText = '';
+    export let required = false;
+</script>
+
+<div class="mb-2">
+	<Label for={id}>{label}</Label>
+	<Input {id} {type} bind:value {placeholder}/>
+	{#if helperText}
+		<div class="text-sm text-gray-500">{helperText}</div>
+	{/if}
+</div>
